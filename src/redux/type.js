e48b55287ed31980176f98ac933dbdbce37f07f0
MH Alikhani <mh.alikhani.07@gmail.com>
2021-08-11 05:43:22 +0430

    built version of app

--- src/redux/type.js
+++
@@ -1,5 +0,0 @@
-export const SET_USER = "SET_USER";
-export const SET_USERS = "SET_USERS";
-export const CLR_INPUT = "CLR_INPUT";
-export const SET_LOADING = "SET_LOADING";
-export const SET_INPUT_VALUE = "SET_INPUT_VALUE";
